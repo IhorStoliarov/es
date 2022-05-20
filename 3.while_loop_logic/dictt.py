3990097fb32a2ab32a735da5e25e89c172d9c97b
Ihor Stoliarov <terema13@gmail.com>
2022-05-20 20:10:29 +0300

    Закінчив словник

--- 3.while_loop_logic/dictt.py
+++ 3.while_loop_logic/dictt.py
@@ -3,10 +3,10 @@
 
 # стоворення словника
 players = {
-            'Olya': 3481,
-            'Vova': 2931,
-            'Ihor': 1331
-            }
+    'Olya': 3481,
+    'Vova': 2931,
+    'Ihor': 1331
+}
 print(players)
 
 # або ще варіант
@@ -29,14 +29,45 @@ del players['Nit']
 print(players)
 
 # перегляд ключів словника
-key = players.keys()
-print(key)
+keys = players.keys()
+print(keys)
 
 # конвертування словника в лист
-l = list(players1.keys())
+l = list(players.keys())
 print(type(l))
 print(l)
 
-# сортування елементів в словнику
-sorted(players1.keys())
-print(players1.keys())
+# сортування елементів в словнику. На виході отримуємо відсортований список
+sorted(players.keys())
+print(sorted(players.keys()))
+
+# перевірка чи існує/не існує ключ в словнику (виводить True)
+print('Olya' in players)
+print('Olka' not in players)
+
+# перевірка ЗНАЧЕННЯ в словнику
+val = players.values()
+print(val)
+
+# конвертування словника в список і вивід значення
+vall = list(players.values())
+print(type(vall))
+print(vall)
+
+# зробити копію листа
+playersCopy = players1.copy()
+print(playersCopy)
+
+# пройти циклом for  по словнику і вивести ключ/значення
+for k, v in players1.items():
+    print(k, v)
+
+# видалити ключ та значення із словника
+playersCopy.pop('Yulya')
+print(playersCopy)
+
+# додавання ключа в словник із ключем None
+playersCopy.setdefault('Ira')
+print(playersCopy)
+
+# словник не дозволяє зробити два однакових ключа з однаковими значеннями!!!
