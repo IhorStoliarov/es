0acf860c9c055dc84f2b376872012bb26c6653f8
Ihor Stoliarov <terema13@gmail.com>
2022-05-21 21:58:02 +0300

    порівняння

--- 2.Elementary/6_format_str.py
+++ 2.Elementary/6_format_str.py
@@ -1,37 +1,14 @@
+# вставлення елементів в рядок. Скільки буде плейсхолдерів ({}) стільки і має бути замінників
+sw = 'Ihor'
+sq = 'python'
+print('my name is {} and i`m learning {}'.format(sw, sq))
 
+# скорочення розрядів до двох після крапки
+pi = 3.1415
+print('Number Pi = {pi:1.2f}'.format(pi=pi))
+# .2f це - флоат, з округленням до двох знаків після крапки
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# більш скорочений синтаксис першого прикладу
+print(f'Мене звуть {sw} і я вивчаю {sq}')
 
 # https://pythonworld.ru/osnovy/formatirovanie-strok-metod-format.html
