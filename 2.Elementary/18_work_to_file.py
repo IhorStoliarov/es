cbc98b3e4c9a06616c61071892d775dfaabdd35b
Ihor Stoliarov <terema13@gmail.com>
2022-05-24 21:30:57 +0300

    ренейм файлів під курс

---
+++ 2.Elementary/18_work_to_file.py
@@ -0,0 +1,3 @@
+# відкрити файл в поточній теці
+file = open('ovk_choven.txt',)
+print(file)
---
+++ 2.Elementary/tst.py
@@ -0,0 +1,5 @@
+def number_to_string(num):
+    return num
+    a = int(input())
+    b = int(input())
+    c = int(a, b)
