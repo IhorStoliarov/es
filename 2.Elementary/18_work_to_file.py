02b210e4f046510112bfda09f2a2b4b3e165abcb
Ihor Stoliarov <terema13@gmail.com>
2022-05-24 22:30:21 +0300

    почав роботу з файлами не за курсом

--- 2.Elementary/18_work_to_file.py
+++ 2.Elementary/18_work_to_file.py
@@ -1,3 +1,12 @@
-# відкрити файл в поточній теці
-file = open('ovk_choven.txt',)
-print(file)
+# відкрити файл в поточній теці в режимі читання в кодуванні UTF-8!!!
+file_ovk = open('ovk_choven.txt', encoding="utf-8")
+# виводимо інфу про нього
+print(file_ovk)
+
+# вивести вміст файла і закрити його
+data = file_ovk.read()
+print(data)
+file_ovk.close()
+
+# створюємо файл в поточній теці
+creat_file = open('wr_txt.txt', 'w')
