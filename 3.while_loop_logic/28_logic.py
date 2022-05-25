3a0def7b260e04a5aa6ecf683469c4a8d27b5c13
Ihor Stoliarov <terema13@gmail.com>
2022-05-25 21:42:12 +0300

    logic operator

---
+++ 3.while_loop_logic/28_logic.py
@@ -0,0 +1,8 @@
+# логіка з умовами (логічні оператори) if, elif, else. True i False пишуться з великої
+a = int(input('Введи число '))
+if a == 1:
+    print('a = 1')
+elif a == 2:
+    print('a = 2')
+else:
+    print('далі я не вмію  рахувати')
