0cc1b99e6e8b81db92b6bfdde929ff6542e026fb
Ihor Stoliarov <terema13@gmail.com>
2022-05-30 13:47:28 +0300

    спроба 2 частково працюкінцевий результат працює, проміжний ні

--- 3.while_loop_logic/hw/34_3_hw_sum_3_5.py
+++ 3.while_loop_logic/hw/34_3_hw_sum_3_5.py
@@ -1,12 +1,12 @@
 limit = int(input('Enter number - '))
 x = int()
 y = int()
-for i in range(limit + 1):
+for i in range(0, limit + 1):
     if i % 3 == 0:
-        x = i
-        i += 1
+        x += i
+        print(int(x))
     elif i % 5 == 0:
-        y = i
-        i += 1
+        y += i
+        print(int(y))
 print(int(x + y))
     
