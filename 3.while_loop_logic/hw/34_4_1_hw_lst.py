8d6af004d59cdef9c8aeda5e3d30f85a7f2eca3f
Ihor Stoliarov <terema13@gmail.com>
2022-05-31 13:41:51 +0300

    done hw 4

--- 3.while_loop_logic/hw/34_4_1_hw_lst.py
+++ 3.while_loop_logic/hw/34_4_1_hw_lst.py
@@ -1,4 +1,14 @@
 import sys
 
-first_lst = sys.argv[1]
-second_lst = sys.argv[2]
+first_lst = [1, 5, 4, 3, 2, 6, 8, 7]
+second_lst = [3, 7, 4, 5, 8, 9, 7, 6]
+lst_x = []
+lst_y = []
+for x in first_lst:
+    if x % 2 != 0:
+        lst_x.append(x)
+for y in second_lst:
+    if y % 2 == 0:
+        lst_y.append(y)
+joined_list = lst_x + lst_y
+print(joined_list)
