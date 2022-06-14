4ca48f2ae8426c4068ccfda2ec28212fc3d48fe8
Ihor Stoliarov <terema13@gmail.com>
2022-06-14 15:48:52 +0300

    трохи функцій

---
+++ 2.Elementary/myHS4/tst_def.py
@@ -0,0 +1,21 @@
+def summa(a, b):
+    return int(a + b)
+
+
+print(summa(int(input('enter first number - ')), int(input('enter second number - '))))
+
+
+def mnoj(k, v):
+    return k * v
+
+
+print(mnoj(int(input('enter first number - ')), int(input('enter second number - '))))
+
+
+def dell(r, t):
+    return r / t
+
+
+print(dell(int(input('enter first number - ')), int(input('enter second number - '))))
+
+
--- 2.Elementary/tst.py
+++ 2.Elementary/tst.py
@@ -1,5 +1,5 @@
 log_file = open('../files_hw/log_win.txt', encoding="utf-8")
-
+print(log_file)
 date_log_file = log_file.read()
 print(date_log_file)
 log_file.close()
