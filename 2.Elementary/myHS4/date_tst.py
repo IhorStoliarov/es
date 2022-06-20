34bbaecc8d7080838f9435291e961967c671cdac
Ihor Stoliarov <terema13@gmail.com>
2022-06-20 17:09:44 +0300

    Байти та кодування (ascii, utf-8, utf-16)

---
+++ 2.Elementary/myHS4/date_tst.py
@@ -0,0 +1,11 @@
+import datetime
+
+dt = [datetime]
+print(dt)
+max = datetime.date(9999, 12, 31)
+min = datetime.date(1, 1, 1)
+print(max)
+print(min)
+resolution = datetime.timedelta(days=1)
+
+print(resolution)
